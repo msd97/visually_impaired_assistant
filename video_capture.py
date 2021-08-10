bda87fc8713b7f3f6c641b80f8474f28e00f7191
msd97 <mariosilvestre2097@gmail.com>
2021-08-10 10:01:23 +0200

    Final commit

--- video_capture.py
+++ video_capture.py
@@ -10,7 +10,7 @@ from ssd_tf_hub import inference_hub, draw_boxes
 from mono_est import depth_est
 
 calc_depth = 1
-edge = 1
+edge = 0
 
 logging.basicConfig(format="%(asctime)s // %(levelname)s : %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
@@ -96,81 +96,89 @@ if not edge:
         for i, line in enumerate(classes):
             labels.append(line.strip())
 
-  
-while(True):
-    
-    # Capture the video frame
-    # by frame
-    ret, frame = cap.read()
+fps_array = []
 
-    new_frame_time = time.time()
-
-    fps = 1/(new_frame_time-prev_frame_time)
-    prev_frame_time = new_frame_time
-
-    if edge:
-        det_frame = cv2.resize(frame.copy(), f_size)
-        image = Image.fromarray(det_frame)
-        _, scale = common.set_resized_input(
-            interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
-        start = time.perf_counter()
-        interpreter.invoke()
-        inference_time = time.perf_counter() - start
-        objs = detect.get_objects(interpreter, 0.4, scale)
-        logging.info('%.2f ms' % (inference_time * 1000))
-        boxes = []
-        class_names = []
-        scores = []
-        for obj in objs:
-            logging.info(labels.get(obj.id, obj.id))
-            logging.info('  id:    {}'.format(obj.id))
-            logging.info('  score: {}'.format(obj.score))
-            logging.info('  bbox:  {}'.format(obj.bbox))
-            class_names.append(obj.id)
-            scores.append(obj.score)
-            xmin = obj.bbox[0] / f_size[0]
-            ymin = obj.bbox[1] / f_size[1]
-            xmax = obj.bbox[2] / f_size[0]
-            ymax = obj.bbox[3] / f_size[1]
-             
-            boxes.append([ymin, xmin, ymax, xmax])
+try:  
+    while(True):
         
-        detection, raw_bbox, scaled_bbox = draw_boxes(det_frame, boxes, class_names, scores, labels, edge=True)
+        # Capture the video frame
+        # by frame
+        ret, frame = cap.read()
 
-    else:
-        detection, raw_bbox, scaled_bbox = inference_hub(cv2.resize(frame.copy(), f_size), model, labels)
+        new_frame_time = time.time()
 
-    if calc_depth:
-        d_map_show, d_map = depth_est(d_model, frame.copy())
-        for i, box in enumerate(raw_bbox):
-            H= d_map.shape[0]
-            W = d_map.shape[1]
-            ymin, xmin, ymax, xmax = tuple(box)
-            xmin = int(W * xmin)
-            ymin = int(H * ymin)
-            xmax = int(W * xmax)
-            ymax = int(H * ymax)
-            avg_depth = np.mean(d_map[ymin:ymax, xmin:xmax])
-            #avg_depth = d_map[int((ymax-ymin)/2), int((xmax-xmin)/2), 0]
-            ymin, xmin, ymax, xmax = tuple(scaled_bbox[i])
-            if 1/avg_depth > 2.5:
-                cv2.putText(detection, 'TOO CLOSE', (xmin, ymax-6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
-        cv2.imshow('depth', d_map_show)
-    #detection = cv2.cvtColor(detection, cv2.COLOR_RGB2BGR)
-    # Display the resulting frame
-    cv2.putText(detection, 'FPS: {:.2f}'.format(fps), (0, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
-    cv2.imshow('frame', detection)
-    out_detection.write(detection)
-    out_map.write(d_map_show)
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-# After the loop release the cap object
-cap.release()
-out_detection.release()
-out_map.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps_array.append(fps)
+
+        if edge:
+            det_frame = cv2.resize(frame.copy(), f_size)
+            image = Image.fromarray(det_frame)
+            _, scale = common.set_resized_input(
+                interpreter, image.size, lambda size: image.resize(size, Image.ANTIALIAS))
+            start = time.perf_counter()
+            interpreter.invoke()
+            inference_time = time.perf_counter() - start
+            objs = detect.get_objects(interpreter, 0.4, scale)
+            logging.info('%.2f ms' % (inference_time * 1000))
+            boxes = []
+            class_names = []
+            scores = []
+            for obj in objs:
+                logging.info(labels.get(obj.id, obj.id))
+                logging.info('  id:    {}'.format(obj.id))
+                logging.info('  score: {}'.format(obj.score))
+                logging.info('  bbox:  {}'.format(obj.bbox))
+                class_names.append(obj.id)
+                scores.append(obj.score)
+                xmin = obj.bbox[0] / f_size[0]
+                ymin = obj.bbox[1] / f_size[1]
+                xmax = obj.bbox[2] / f_size[0]
+                ymax = obj.bbox[3] / f_size[1]
+                
+                boxes.append([ymin, xmin, ymax, xmax])
+            
+            detection, raw_bbox, scaled_bbox = draw_boxes(det_frame, boxes, class_names, scores, labels, edge=True)
+
+        else:
+            detection, raw_bbox, scaled_bbox = inference_hub(cv2.resize(frame.copy(), f_size), model, labels)
+
+        if calc_depth:
+            d_map_show, d_map = depth_est(d_model, frame.copy())
+            for i, box in enumerate(raw_bbox):
+                H= d_map.shape[0]
+                W = d_map.shape[1]
+                ymin, xmin, ymax, xmax = tuple(box)
+                xmin = int(W * xmin)
+                ymin = int(H * ymin)
+                xmax = int(W * xmax)
+                ymax = int(H * ymax)
+                avg_depth = np.mean(d_map[ymin:ymax, xmin:xmax])
+                #avg_depth = d_map[int((ymax-ymin)/2), int((xmax-xmin)/2), 0]
+                ymin, xmin, ymax, xmax = tuple(scaled_bbox[i])
+                if 1/avg_depth > 2.5:
+                    cv2.putText(detection, 'TOO CLOSE', (xmin, ymax-6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+            cv2.imshow('depth', d_map_show)
+            out_map.write(d_map_show)
+        #detection = cv2.cvtColor(detection, cv2.COLOR_RGB2BGR)
+        # Display the resulting frame
+        cv2.putText(detection, 'FPS: {:.2f}'.format(fps), (0, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+        cv2.imshow('frame', detection)
+        out_detection.write(detection)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            raise AttributeError
+            #break
+except AttributeError:
+
+    fps_array = np.array(fps_array)
+    logging.info('Average FPS count: {:.2f}'.format(fps_array.mean()))  
+    # After the loop release the cap object
+    logging.info('Cleaning up and exiting')
+    cap.release()
+    out_detection.release()
+    out_map.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
